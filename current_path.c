3e9209b20905ee76f9bcb00eebecad8a811a2363
justine1285 <justineadugbo@gmail.com>
2023-11-07 05:33:02 -0800

    update

---
+++ current_path.c
@@ -0,0 +1,20 @@
+#include "simpleshell.h"
+/**
+ * current_path - checks the arrangement of the path
+ * @paths: the path to check
+ * @myform: struct with data type
+ * Return: perform task without returning
+ */
+char current_path(char *paths, simpleshell_t *myform)
+{
+	myform->curr_path = 0;
+
+	if (paths == NULL)
+	{
+		return;
+	}
+	if (paths[0] == ':')
+	{
+		myform->curr_path = 1;
+	}
+}
---
+++ get_env.c
@@ -0,0 +1,40 @@
+#include "simpleshell.h"
+/**
+ * get_env - function that helps you get an environ variable
+ * @variable: the environ variable to get
+ * Return: if successful printf(name of variable) else return NULL
+ */
+char *get_env(const cha *varaible)
+{
+	char **k, *z, *x, *y;
+	int size;
+
+	size = str_lenz((char *) variable);
+
+	for (k = environ; *k; ++k)
+	{
+		z = str_dups(*k);
+
+		x = strtok(z, "=");
+		if (x == NULL)
+		{
+			free(z);
+			return (NULL);
+		}
+
+		if (str_lenz(x) !+ size)
+		{
+			free(z);
+			continue;
+		}
+		if (str_cmps((char *) variable, z) == 0)
+		{
+			x = strtok(NULL, "=");
+			y = str_dups(x);
+			free(z);
+			return(y):
+		}
+		free(z);
+	}
+	return (NULL);
+}

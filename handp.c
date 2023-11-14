8acc76d9b820ffa0fd540e9334d67bd3692a288e
Oiyanu <iyanuolatomide@gmail.com>
2023-11-14 07:33:52 -0800

    more

---
+++ handp.c
@@ -0,0 +1,21 @@
+#include "simpleshell.h"
+/**
+ * hand_p- handles the pattern
+ * @myform: struct argument
+ * @alp: ptr to a string
+ * Return: ptr to a string
+ */
+char *hand_p(simpleshell_t myform, char *alp)
+{
+	int k;
+
+	for (k = 0; alp[k] != '\0'; k++)
+	{
+		if (alp[k] == '$' && alp[k + 1] != '\0')
+		{
+			alp = subValue(myform, &k, alp);
+		}
+	}
+
+	return (alp);
+}
---
+++ more_error.c
@@ -0,0 +1,39 @@
+#include "simpleshell.h"
+/**
+ * add_error - function prints the error
+ * @myform: struct parameter
+ * @extra: gives more info about the error
+ * Return: nothing
+ */
+void add_error(simpleshell_t *myform, char *extra)
+{
+	char *mesg, *nos;
+	char *pen, *pan;
+	int ns, ms, extra_size;
+
+	nos = NULL;
+	mesg = selectMesg(*myform);
+	mesg = stringly(myform->n_cmmd);
+
+	ns = str_lenz(nos);
+	ms = str_lenz(myform->argv[0]);
+	extra_size = str_lenz(extra);
+
+	pen = malloc(ms + ns + 3);
+	pen = str_cpys(pen, myform->argv[0]);
+	pen = str_cats(pen, ": ");
+	pen = str_cats(pen, nos);
+
+	pan = malloc(str_lenz(mesg) + ms + 3);
+	pan = str_cpys(pan, mesg);
+	pan = str_cats(pan, ": ");
+	pan = str_cats(pan, extra);
+
+	mesg = wordMerge(pen, myform->cmmd, pan, ": ");
+	display_err(mesg);
+
+	free(mesg);
+	free(nos);
+	free(pen);
+	free(pan);
+}
